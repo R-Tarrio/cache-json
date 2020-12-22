d30dc9479232e16cf63d3e4ef91503200180cfb6
Rodrigo Tarrio <rodrigotarrio2@gmail.com>
2020-12-22 16:48:01 -0400

    Creando carpetas para archivos cache

--- src/cacheJson.php
+++ src/cacheJson.php
@@ -3,8 +3,32 @@ namespace cacheJson\cacheJson;
 
 class cacheJson
 {
-    public function sum($a, $b)
+
+	public $HASH = null;
+	public $RUTA = __DIR__."/cache/json/";
+	public $FILE = "cache.json";
+	public $FILE_CACHE = "cache.json";
+	public $data = [];
+
+	function __construct($hash)
+	{
+		$this->HASH = $hash;
+		$this->FILE = $this->HASH.".json";
+		$this->FILE_CACHE = $this->RUTA.$this->FILE;
+	}
+
+    public function setCache($data)
     {
-        return $a + $b;
+    	return file_put_contents($this->FILE_CACHE, json_encode($data));
     }
+
+    public function getCache()
+    {
+    	if(file_exists($this->FILE_CACHE)){
+	    	return file_get_contents($this->FILE_CACHE);
+    	}else{
+    		return [];
+    	}
+    }
+
 }
